99d8212dbe3796d74b71269704b0267e0be4800d
cattoa <allistair@bobcatt.co.za>
2016-06-22 06:05:02 +0200

    Refining document and paragraph

--- lib/wordx/document.rb
+++ lib/wordx/document.rb
@@ -3,43 +3,72 @@ require "wordx/styles"
 module Wordx
   class Document
     def initialize()
-      @@current_paragraph = -1
-      @@paragraphs = []
+      @@current_paragraph = 0
+      if !defined? @@paragraphs
+        @@paragraphs = {}
+      end
+    end
 
+    def list
+      @@paragraphs.keys
     end
 
     def new_paragraph(text, Wordx::Style style, font, size)
-      paragraph = Paragragh.new(text, style, font, size)
-      @@paragraphs << paragraph
-      @@current_paragraph = @@paragraphs.length - 1
+      para_key = get_next_para_key()
+      paragraph = Paragragh.new(para_key,text, style, font, size)
+      @@paragraphs[para_key] =  paragraph
     end
 
-    def change_paragraph_text(text)
-      paragraph = @@paragraphs[@@current_paragraph]
-      paragraph.text(text)
+    def change_paragraph_text(text, key=nil)
+      if key.nil?
+        key = get_current_para_key
+      end
+      paragraph = @@paragraphs[key]
+      paragraph.text(text) unless paragraph.nil?
     end
 
-    def change_paragraph_style(Wordx::Style style)
-      paragraph = @@paragraphs[@@current_paragraph]
-      paragraph.style(style)
+    def change_paragraph_style(Wordx::Style style, key=nil)
+      if key.nil?
+        key = get_current_para_key
+      end
+      paragraph = @@paragraphs[key]
+      paragraph.style(style) unless paragraph.nil?
     end
 
-    def change_paragraph_font(font)
-      paragraph = @@paragraphs[@@current_paragraph]
-      paragraph.font(font)
+    def change_paragraph_font(font, key=nil)
+      if key.nil?
+        key = get_current_para_key
+      end
+      paragraph = @@paragraphs[key]
+      paragraph.font(font) unless paragraph.nil?
     end
 
-    def change_paragraph_size(size)
-      paragraph = @@paragraphs[@@current_paragraph]
-      paragraph.size(size)
+    def change_paragraph_size(size, key=nil)
+      if key.nil?
+        key = get_current_para_key
+      end
+      paragraph = @@paragraphs[key]
+      paragraph.size(size) unless paragraph.nil?
+    end
+
+    private
+    def get_next_para_key()
+      @@current_paragraph++
+      next_count = get_current_para()
+    end
+
+    def get_current_para_key()
+      current_para = "para_" + @@current_paragraph.to_s.rjust(3,'0').to_sym
     end
 
   end
 
   class Paragraph
-    attr_accessor :text, :style, :font, :size
+    attr_accessor :key, :text, :style, :font, :size
 
-    def initialize(text, Wordx::Style style, font, size)
+
+    def initialize(key, text, Wordx::Style style, font, size)
+      @@key = key
       @@text = text
       @@style = style
       @@font = font
