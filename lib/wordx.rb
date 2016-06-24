11a9970b683c159ac9a4ec760aa078523757e6fa
cattoa <allistair@bobcatt.co.za>
2016-06-24 02:57:46 +0200

    Added new tests to rspec

--- lib/wordx.rb
+++ lib/wordx.rb
@@ -7,29 +7,30 @@ require "wordx/rels"
 
 
 module Wordx
-  def initialize()
-    styles = Wordx::Styles.new()
+  class Documents
+    def initialize()
+      @@styles = Wordx::Styles.new()
+    end
 
-  end
+    def list_styles()
+      @@styles.list
+    end
 
-  def document()
-
-  end
-
-  def create_document()
-    content_type = Wordx::ContentType.new()
-    content_type.create()
-    doc_props_app = Wordx::DocProp.new()
-    doc_props_app.create_app()
-    doc_props_app.create_core()
-    rels = Wordx::Rels.new()
-    rels.create_rels()
-    word = Wordx::Word.new()
-    word.create_font_table()
-    word.create_rels()
-    word.create_settings()
-    word.create_styles()
-    word.create_numbering()
-    word.create_document()
+    def create_document()
+      content_type = Wordx::ContentType.new()
+      content_type.create()
+      doc_props_app = Wordx::DocProp.new()
+      doc_props_app.create_app()
+      doc_props_app.create_core()
+      rels = Wordx::Rels.new()
+      rels.create_rels()
+      word = Wordx::Word.new()
+      word.create_font_table()
+      word.create_rels()
+      word.create_settings()
+      word.create_styles()
+      word.create_numbering()
+      word.create_document()
+    end
   end
 end
--- spec/wordx_spec.rb
+++ spec/wordx_spec.rb
@@ -1,12 +1,31 @@
 require 'spec_helper'
 require 'wordx'
 
-describe Wordx do
-  it 'has a version number' do
-    expect(Wordx::VERSION).not_to be nil
+describe '.Wordx' do
+
+  it 'version number is 0.0.1' do
+    expect(Wordx::VERSION).to eq '0.0.1'
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  before :each do
+    @documents = Wordx::Documents.new()
+    @styles = @documents.list_styles()
   end
+
+  it 'takes no parameters and returns a Wordx::Documents object' do
+      expect(@documents).to be_an_instance_of Wordx::Documents
+  end
+
+  it 'documents should contain 2 Wordx::Styles' do
+    expect(@styles.count).to eq 2
+  end
+
+  it 'style should contain DefaultText' do
+    expect(@styles).to include(:DefaultText)
+  end
+
+  it 'style should contain Normal' do
+    expect(@styles).to include(:Normal)
+  end
+
 end
